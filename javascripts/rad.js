ff66a19543aedf6f5b2716d7a125a0f490253f52
arpan <arpan@gmail.com>
2020-09-15 21:07:25 +1200

    bs4

---
+++ javascripts/rad.js
@@ -0,0 +1,22 @@
+
+
+
+var live = document.getElementById("myLive"); 
+
+function play() { 
+  live.play(); 
+  document.getElementById('play-img').src="/images/pause.png";
+  
+  
+} 
+
+function pause() { 
+
+  live.pause();
+  document.getElementById('play-img').src="/images/play.png";
+
+} 
+
+ function myPlay(){
+    live.pause();
+} 
